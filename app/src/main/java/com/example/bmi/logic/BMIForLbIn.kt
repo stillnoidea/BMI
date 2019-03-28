85d5b9aab30bfc55cb6a5fa16f7ed80caaff807d
Urszula Mazur <urszula.mazur@mail.com>
2019-03-28 18:13:58 +0100

    Validating input fix

--- app/src/main/java/com/example/bmi/logic/BMIForLbIn.kt
+++ app/src/main/java/com/example/bmi/logic/BMIForLbIn.kt
@@ -5,10 +5,10 @@ class BMIForLbIn(private var weight: Int, private var height: Int) : Bmi {
     override fun countBmi(): Double = (weight / (height.toDouble() * height)) * 703.0
 
     override fun isInRangeWeight(): Boolean {
-        return (1763 > weight || weight > 66)
+        return (weight in 67..1762)
     }
 
     override fun isInRangeHeight(): Boolean {
-        return (102 > height || height > 39)
+        return (height in 40..101)
     }
 }
--- app/src/main/java/com/example/bmi/logic/BMiForKgCm.kt
+++ app/src/main/java/com/example/bmi/logic/BMiForKgCm.kt
@@ -5,11 +5,11 @@ class BMiForKgCm(private var weight: Int, private var height: Int) : Bmi {
     override fun countBmi(): Double = weight * 10000.0 / (height * height)
 
     override fun isInRangeWeight():Boolean{
-        return (800 > weight || weight > 30 )
+        return (weight in 31..799)
     }
 
     override fun isInRangeHeight():Boolean{
-        return ( 260 > height || height > 100)
+        return (height in 101..259)
     }
 
 }
